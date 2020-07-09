9b987237e3562d7a6fceca13b65a0fae7847679f
daee <thundrnuts@gmail.com>
2020-07-08 21:31:25 -0700

    Initial Commit

---
+++ fast jack/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fast jack
+//
+//  Created by Daee Kang on 7/8/20.
+//  Copyright © 2020 Daee Kang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
